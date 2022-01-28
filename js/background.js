91bb3c3be0421f0cf7288994e0e0458df415ede7
danheon kim <eksgjs3847@naver.com>
2022-01-28 10:36:43 +0900

    fix a error

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(../img/${chosenImage})`
+document.body.style.backgroundImage = `url(img/${chosenImage})`
